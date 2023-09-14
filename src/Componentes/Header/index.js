91c2eb6ee8e586814635d0789afecb9638518b97
Raimundo <raimundo_nn16@hotmail.com>
2023-09-13 22:01:01 -0300

    mudando estilos com styled

--- src/Componentes/Header/index.js
+++ src/Componentes/Header/index.js
@@ -1,10 +1,37 @@
-import './Estilo.css'
+import styled from 'styled-components'
+
+const CabecalhoStyle = styled.header`
+    color: black;
+    font-family: 'Aboreto';
+    text-align: center;
+    margin: 5% 0% 5% 0%;
+    font-size: 20px;
+    h1 {
+        border-bottom: 1px solid black;
+        margin: 0% 35% 0% 35%;
+    }
+    @media (max-width: 1368px) {
+    h1 {
+        font-size: 2rem;
+    }
+}
+    @media (max-width: 768px) {
+    h1 {
+        font-size: 18px;
+    }
+}
+    @media (max-width: 480px) {
+    h1 {
+        font-size: 20px;
+    }
+}
+`
 
 function Cabecalho() {
     return (
-        <header className="cabecalho">
+        <CabecalhoStyle>
             <h1>Meus Projetos</h1>
-        </header>
+        </CabecalhoStyle>
     )
 }
 
